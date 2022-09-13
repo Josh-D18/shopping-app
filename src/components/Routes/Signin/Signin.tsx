f5e12558ff9ffb945155793c1293045cbaa2dddd
Josh D <josh.date@techguilds.com>
2022-09-12 23:21:26 -0400

    added env file and create user doc phase 1

--- src/components/Routes/Signin/Signin.tsx
+++ src/components/Routes/Signin/Signin.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils";
 
 const Signin = () => {
-  return <div>Sign In Page</div>;
+  const logGoogleUser = async () => {
+    const { user } = await signInWithGooglePopup();
+    createUserDocumentFromAuth(user);
+    console.log(user);
+  };
+
+  return (
+    <div>
+      Sign In Page
+      <button onClick={logGoogleUser}>Sign In with Google</button>
+    </div>
+  );
 };
 
 export default Signin;
--- src/components/utils/firebase/firebase.utils.tsx
+++ src/components/utils/firebase/firebase.utils.tsx
@@ -1,9 +1,41 @@
 import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
-// const firebase.utils = () => {
-//   return (
-//     <div>firebase.utils</div>
-//   )
-// }
+const firebaseConfig = {
+  apiKey: "AIzaSyCa4a3kUTxmD43B47NINto_wJ9kwBrPv7k",
+  authDomain: "shopping-app-db-40d09.firebaseapp.com",
+  projectId: "shopping-app-db-40d09",
+  storageBucket: "shopping-app-db-40d09.appspot.com",
+  messagingSenderId: "474195402195",
+  appId: "1:474195402195:web:fa54950fb4c6f67921a326",
+};
 
-// export default firebase.utils
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+
+// Auth
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+// DB
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth: any) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot.exists());
+};
